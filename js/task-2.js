fefeba4cce66f82a4a947b1ca45b0f9983fe8df7
acenickace <acenickace53@gmail.com>
2026-02-04 12:21:48 +0200

    refactor task2

--- js/task-2.js
+++ js/task-2.js
@@ -13,8 +13,9 @@ class Storage {
   }
 
   removeItem(itemToRemove) {
-    if (this.#items.indexOf(itemToRemove) != -1) {
-      this.#items.splice(this.#items.indexOf(itemToRemove), 1);
+    const indexToRemove = this.#items.indexOf(itemToRemove);
+    if (indexToRemove !== -1) {
+      this.#items.splice(indexToRemove, 1);
     }
   }
 }
